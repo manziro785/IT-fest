341da524f4f49235e98491fb4010e87a10efca68
Akbar <iron.man96321@gmail.com>
2024-05-24 15:48:47 +0600

    coruse

--- IT_FEST/src/pages/mainPage/mainPage.jsx
+++ IT_FEST/src/pages/mainPage/mainPage.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Partner from '../../components/partner/partner';
 import Sponsors from '../../components/sponsors/sponsors';
 
 const MainPage = () => {
     return (
         <>
              main page
-<Sponsors/>
-             <Partner/>
+
+             <Sponsors/>
         </>
     );
 };
